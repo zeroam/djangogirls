c1b2d41735733de5f6687910216228ca5c2e17c4
jone <imdff0803@gmail.com>
2019-01-25 15:30:51 +0900

    add migrations

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,6 @@ from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
 
 def post_list(request):
-    print('hello')
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
--- compose/django/start-dev.sh
+++ compose/django/start-dev.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-python manage.py makemigration blog
+python manage.py makemigrations blog
 python manage.py migrate
 python manage.py runserver 0:8000
